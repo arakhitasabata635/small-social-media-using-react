67947c8c485f1931519a4580fc2a11068728cf90
arakhita <arakhitaSabata635@gmail.com>
2025-09-11 16:01:09 +0530

    add loading effect

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,26 +1,29 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostListContext } from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
+import LoadingSpiner from "./LoadingSpiner";
 
 const PostList = () => {
   const { postList, addInitialPostList } = useContext(PostListContext);
 
-  const onGetPostClicked = () => {
+  const [featching, setFatching]= useState(false)
+
+  useEffect(() => {
+    setFatching(true)
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then((data) => {
         addInitialPostList(data.posts);
-        console.log(data.posts);
+        setFatching(false)
       });
-  };
+  }, []);
 
   return (
     <>
-      {postList.length === 0 && (
-        <WelcomeMessage onGetPostClicked={onGetPostClicked}></WelcomeMessage>
-      )}
-      {postList.map((post) => (
+    {featching && <LoadingSpiner/>}
+      {!featching && postList.length === 0 && <WelcomeMessage></WelcomeMessage>}
+      {!featching && postList.map((post) => (
         <Post key={post.id} post={post} />
       ))}
     </>
--- src/components/WelcomeMessage.jsx
+++ src/components/WelcomeMessage.jsx
@@ -1,17 +1,7 @@
-
-
-const WelcomeMessage = ({onGetPostClicked}) => {
-
+const WelcomeMessage = ({ onGetPostClicked }) => {
   return (
     <center className="Welcome-message">
       <h1>There are no posts</h1>
-      <button
-        type="button"
-        onClick={onGetPostClicked}
-        className="btn btn-primary"
-      >
-        Get Post From Server
-      </button>
     </center>
   );
 };
