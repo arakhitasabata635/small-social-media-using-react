e39a454d72f250ff03ea72a8d64e28e580d3aa46
arakhita <arakhitaSabata635@gmail.com>
2025-09-13 20:05:10 +0530

    improve some css

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -13,7 +13,7 @@ const Post = ({ post }) => {
         <h5 className="card-title">
           {post.title}
 
-          <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
+          <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger delete"
           
           onClick={()=>{deletePost(post.id)}}
           >
